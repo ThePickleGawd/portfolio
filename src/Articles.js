f89c00c15374922c1b8ad264e85ae759558ec95c
Dylan <dylanelu@gmail.com>
2020-08-31 10:18:21 -0700

    init

---
+++ src/Articles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Articles = (props) => {
+  return (
+    <div>
+      <h1>New Component</h1>
+    </div>
+  );
+};
+
+export default Articles;
---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Thumbnail from "./Thumbnail.js";
+
+const Projects = (props) => {
+  return (
+    <div>
+      <Thumbnail title="test" category="test category" />
+    </div>
+  );
+};
+
+export default Projects;
