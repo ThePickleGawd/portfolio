38c1a8dfeecb545c1d3c3318c79eead273e8b6db
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-08-14 12:10:46 -0700

    cool image effects

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,10 @@ import { useOnScreen, useHashCheck } from "../util/helper";
 // Pictures
 import profilePic from "../public/me.jpg";
 import unityLogo from "../public/unity.png";
+import reactLogo from "../public/react.png";
+import nextLogo from "../public/next.png";
+import cppLogo from "../public/cpp.png";
+import stars from "../public/stars.png";
 
 export const Home = () => {
   const parallaxRef = useRef();
@@ -27,7 +31,7 @@ export const Home = () => {
   const hashCheck = useHashCheck(parallaxRef);
 
   return (
-    <Parallax pages={3} style={{ top: "0", left: "0" }} ref={parallaxRef}>
+    <Parallax pages={5} style={{ top: "0", left: "0" }} ref={parallaxRef}>
       <ParallaxLayer
         offset={0}
         speed={2.5}
@@ -73,6 +77,16 @@ export const Home = () => {
         <About />
       </ParallaxLayer>
       <ParallaxLayer
+        offset={0}
+        speed={0}
+        factor={5}
+        style={{
+          backgroundImage:
+            "url(https://awv3node-homepage.surge.sh/build/assets/stars.svg)",
+          backgroundSize: "cover",
+        }}
+      />
+      <ParallaxLayer
         offset={0.9}
         speed={1.3}
         style={{
@@ -83,6 +97,48 @@ export const Home = () => {
       >
         <div style={{ opacity: 0.3, marginRight: "15%" }}>
           <Image src={unityLogo} alt="Unity Logo" />
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={1}
+        speed={0.3}
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div
+          style={{ opacity: 0.3, marginLeft: "25%", width: 250, height: 250 }}
+        >
+          <Image src={cppLogo} alt="CPP Logo" />
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={1.6}
+        speed={1.3}
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignContent: "center",
+        }}
+      >
+        <div
+          style={{ opacity: 0.3, marginLeft: "5%", width: 250, height: 250 }}
+        >
+          <Image src={reactLogo} alt="React Logo" />
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={1.7}
+        speed={0.6}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <div style={{ opacity: 0.3 }}>
+          <Image src={nextLogo} alt="Next.js Logo" />
         </div>
       </ParallaxLayer>
     </Parallax>
